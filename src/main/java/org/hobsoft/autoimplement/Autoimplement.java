2d75573a7cd6903e29fe1ef7e4db4898c4b9e4f2
Mark Hobson <markhobson@gmail.com>
2018-06-06 21:34:07 +0100

    Extract expression compiler into separate class

--- src/main/java/org/hobsoft/autoimplement/Autoimplement.java
+++ src/main/java/org/hobsoft/autoimplement/Autoimplement.java
@@ -13,26 +13,14 @@
  */
 package org.hobsoft.autoimplement;
 
-import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.hobsoft.autoimplement.example.Calculator;
-import org.joor.Reflect;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ReturnStmt;
 
 import static java.util.stream.Collectors.toList;
-
-import static com.github.javaparser.ast.Modifier.PUBLIC;
-import static com.github.javaparser.ast.type.PrimitiveType.intType;
 
 /**
  * Writes code so you don't have to.
@@ -43,14 +31,14 @@ public class Autoimplement<T>
 {
 	private static final int POPULATION_SIZE = 10;
 	
-	private final TestRunner<T> testRunner;
+	private final ExpressionCompiler<T> expressionCompiler;
 	
-	private int implCount;
+	private final TestRunner<T> testRunner;
 	
 	public Autoimplement(Class<T> implementationClass, Class<?> testClass)
 	{
+		expressionCompiler = new ExpressionCompiler<>();
 		testRunner = new TestRunner<>(implementationClass, testClass);
-		implCount = 0;
 	}
 	
 	public void evolve()
@@ -70,7 +58,6 @@ public class Autoimplement<T>
 	private void evolve(List<Expression> population)
 	{
 		double[] fitness = population.stream()
-			.map(this::createImplementation)
 			.mapToDouble(this::fitness)
 			.toArray();
 		
@@ -81,48 +68,11 @@ public class Autoimplement<T>
 		}
 	}
 	
-	private double fitness(T implementation)
+	private double fitness(Expression expression)
 	{
+		T implementation = expressionCompiler.compile(expression);
 		TestExecutionSummary summary = testRunner.run(implementation);
 		
 		return (double) summary.getTestsSucceededCount() / summary.getTestsFoundCount();
-	}
-	
-	private T createImplementation(Expression expression)
-	{
-		String name = String.format("CalculatorImpl%d", implCount++);
-		return compile(createImplementationUnit(name, expression));
-	}
-	
-	private static CompilationUnit createImplementationUnit(String name, Expression expression)
-	{
-		CompilationUnit unit = new CompilationUnit();
-		
-		ClassOrInterfaceDeclaration type = unit.addClass(name);
-		type.addImplementedType(Calculator.class);
-		
-		MethodDeclaration method = new MethodDeclaration(EnumSet.of(PUBLIC), intType(), "add");
-		method.addParameter(intType(), "x");
-		method.addParameter(intType(), "y");
-		type.addMember(method);
-		
-		BlockStmt body = new BlockStmt();
-		body.addStatement(new ReturnStmt(expression));
-		method.setBody(body);
-		
-		return unit;
-	}
-	
-	private T compile(CompilationUnit unit)
-	{
-		String name = unit.getTypes()
-			.stream()
-			.findFirst()
-			.map(NodeWithSimpleName::getNameAsString)
-			.orElseThrow(() -> new IllegalArgumentException("Missing type"));
-		
-		return Reflect.compile(name, unit.toString())
-			.create()
-			.get();
 	}
 }
---
+++ src/main/java/org/hobsoft/autoimplement/ExpressionCompiler.java
@@ -0,0 +1,65 @@
+package org.hobsoft.autoimplement;
+
+import java.util.EnumSet;
+
+import org.hobsoft.autoimplement.example.Calculator;
+import org.joor.Reflect;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+
+import static com.github.javaparser.ast.Modifier.PUBLIC;
+import static com.github.javaparser.ast.type.PrimitiveType.intType;
+
+public class ExpressionCompiler<T>
+{
+	private int implCount;
+	
+	public ExpressionCompiler()
+	{
+		implCount = 0;
+	}
+	
+	public T compile(Expression expression)
+	{
+		String name = String.format("CalculatorImpl%d", implCount++);
+		return compile(createImplementationUnit(name, expression));
+	}
+	
+	private T compile(CompilationUnit unit)
+	{
+		String name = unit.getTypes()
+			.stream()
+			.findFirst()
+			.map(NodeWithSimpleName::getNameAsString)
+			.orElseThrow(() -> new IllegalArgumentException("Missing type"));
+		
+		return Reflect.compile(name, unit.toString())
+			.create()
+			.get();
+	}
+	
+	private static CompilationUnit createImplementationUnit(String name, Expression expression)
+	{
+		CompilationUnit unit = new CompilationUnit();
+		
+		ClassOrInterfaceDeclaration type = unit.addClass(name);
+		type.addImplementedType(Calculator.class);
+		
+		MethodDeclaration method = new MethodDeclaration(EnumSet.of(PUBLIC), intType(), "add");
+		method.addParameter(intType(), "x");
+		method.addParameter(intType(), "y");
+		type.addMember(method);
+		
+		BlockStmt body = new BlockStmt();
+		body.addStatement(new ReturnStmt(expression));
+		method.setBody(body);
+		
+		return unit;
+	}
+}
