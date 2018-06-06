6e9d411c71790bb1159c06fd27f3ef658fa83739
Mark Hobson <markhobson@gmail.com>
2018-06-06 19:32:03 +0100

    Create initial population and evaluate fitness

--- src/main/java/org/hobsoft/autoimplement/Autoimplement.java
+++ src/main/java/org/hobsoft/autoimplement/Autoimplement.java
@@ -13,12 +13,35 @@
  */
 package org.hobsoft.autoimplement;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import org.hobsoft.autoimplement.example.Calculator;
+import org.joor.Reflect;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.IntegerLiteralExpr;
+import com.github.javaparser.ast.expr.LiteralExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+
+import static java.util.stream.Collectors.toList;
+
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 import static org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder.request;
+
+import static com.github.javaparser.ast.Modifier.PUBLIC;
+import static com.github.javaparser.ast.type.PrimitiveType.intType;
 
 /**
  * Writes code so you don't have to.
@@ -27,17 +50,25 @@ import static org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder.r
  */
 public class Autoimplement<T>
 {
+	private static final int POPULATION_SIZE = 10;
+	
 	private final Class<T> implementationClass;
 	
 	private final Class<?> testClass;
 	
 	private final Launcher launcher;
 	
+	private final Random random;
+	
+	private int implCount;
+	
 	public Autoimplement(Class<T> implementationClass, Class<?> testClass)
 	{
 		this.implementationClass = implementationClass;
 		this.testClass = testClass;
 		launcher = LauncherFactory.create();
+		random = new Random();
+		implCount = 0;
 	}
 	
 	public double evaluate(T implementation)
@@ -49,5 +80,90 @@ public class Autoimplement<T>
 		launcher.execute(request().selectors(selectClass(testClass)).build());
 		
 		return (double) listener.getSummary().getTestsSucceededCount() / listener.getSummary().getTestsFoundCount();
+	}
+	
+	public void evolve()
+	{
+		List<Expression> population = randomPopulation();
+		
+		evolve(population);
+	}
+	
+	private List<Expression> randomPopulation()
+	{
+		return Stream.generate(this::randomExpression)
+			.limit(POPULATION_SIZE)
+			.collect(toList());
+	}
+	
+	private Expression randomExpression()
+	{
+		return (random.nextDouble() < 0.5)
+			? randomLiteral()
+			: randomName();
+	}
+	
+	private LiteralExpr randomLiteral()
+	{
+		return new IntegerLiteralExpr(random.nextInt(10));
+	}
+	
+	private Expression randomName()
+	{
+		return (random.nextDouble() < 0.5)
+			? new NameExpr("x")
+			: new NameExpr("y");
+	}
+	
+	private void evolve(List<Expression> population)
+	{
+		double[] fitness = population.stream()
+			.map(this::createImplementation)
+			.mapToDouble(this::evaluate)
+			.toArray();
+		
+		for (int i = 0; i < population.size(); i++)
+		{
+			System.out.println(population.get(i));
+			System.out.println(fitness[i]);
+		}
+	}
+	
+	private T createImplementation(Expression expression)
+	{
+		String name = String.format("CalculatorImpl%d", implCount++);
+		return compile(createImplementationUnit(name, expression));
+	}
+	
+	private static CompilationUnit createImplementationUnit(String name, Expression expression)
+	{
+		CompilationUnit unit = new CompilationUnit();
+		
+		ClassOrInterfaceDeclaration type = unit.addClass(name);
+		type.addImplementedType(Calculator.class);
+		
+		MethodDeclaration method = new MethodDeclaration(EnumSet.of(PUBLIC), intType(), "add");
+		method.addParameter(intType(), "x");
+		method.addParameter(intType(), "y");
+		type.addMember(method);
+		
+		BlockStmt body = new BlockStmt();
+		body.addStatement(new ReturnStmt(expression));
+		method.setBody(body);
+		
+		return unit;
+	}
+	
+	private T compile(CompilationUnit unit)
+	{
+		String name = unit.getTypes()
+			.stream()
+			.findFirst()
+			.map(NodeWithSimpleName::getNameAsString)
+			.orElseThrow(() -> new IllegalArgumentException("Missing type"));
+		
+		return Reflect.compile(name, unit.toString())
+			.create()
+			.get();
 	}
 }
--- src/main/java/org/hobsoft/autoimplement/Main.java
+++ src/main/java/org/hobsoft/autoimplement/Main.java
@@ -13,26 +13,8 @@
  */
 package org.hobsoft.autoimplement;
 
-import java.util.EnumSet;
-
 import org.hobsoft.autoimplement.example.Calculator;
 import org.hobsoft.autoimplement.example.CalculatorTest;
-import org.joor.Reflect;
-
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.IntegerLiteralExpr;
-import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ReturnStmt;
-
-import static com.github.javaparser.ast.Modifier.PUBLIC;
-import static com.github.javaparser.ast.expr.BinaryExpr.Operator.PLUS;
-import static com.github.javaparser.ast.type.PrimitiveType.intType;
 
 /**
  * Runs Autoimplement against the calculator example.
@@ -43,53 +25,6 @@ public class Main
 	{
 		Autoimplement<Calculator> autoimplement = new Autoimplement<>(Calculator.class, CalculatorTest.class);
 		
-		System.out.println(autoimplement.evaluate(createCalculatorImpl1()));
-		System.out.println(autoimplement.evaluate(createCalculatorImpl2()));
-	}
-	
-	private static Calculator createCalculatorImpl1()
-	{
-		Expression expression = new IntegerLiteralExpr(0);
-		
-		return compile(createCalculatorUnit("CalculatorImpl1", expression));
-	}
-	
-	private static Calculator createCalculatorImpl2()
-	{
-		Expression expression = new BinaryExpr(new NameExpr("x"), new NameExpr("y"), PLUS);
-		
-		return compile(createCalculatorUnit("CalculatorImpl2", expression));
-	}
-	
-	private static CompilationUnit createCalculatorUnit(String name, Expression expression)
-	{
-		CompilationUnit unit = new CompilationUnit();
-		
-		ClassOrInterfaceDeclaration type = unit.addClass(name);
-		type.addImplementedType(Calculator.class);
-		
-		MethodDeclaration method = new MethodDeclaration(EnumSet.of(PUBLIC), intType(), "add");
-		method.addParameter(intType(), "x");
-		method.addParameter(intType(), "y");
-		type.addMember(method);
-		
-		BlockStmt body = new BlockStmt();
-		body.addStatement(new ReturnStmt(expression));
-		method.setBody(body);
-		
-		return unit;
-	}
-	
-	private static Calculator compile(CompilationUnit unit)
-	{
-		String name = unit.getTypes()
-			.stream()
-			.findFirst()
-			.map(NodeWithSimpleName::getNameAsString)
-			.orElseThrow(() -> new IllegalArgumentException("Missing type"));
-		
-		return Reflect.compile(name, unit.toString())
-			.create()
-			.get();
+		autoimplement.evolve();
 	}
 }
