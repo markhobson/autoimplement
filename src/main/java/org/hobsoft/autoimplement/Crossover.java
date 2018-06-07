32800790bd8fada500b134ae54ad3ff63953060f
Mark Hobson <markhobson@gmail.com>
2018-06-07 08:02:41 +0100

    Formatted mutator and crossover

--- src/main/java/org/hobsoft/autoimplement/Crossover.java
+++ src/main/java/org/hobsoft/autoimplement/Crossover.java
@@ -4,21 +4,26 @@ import static org.hobsoft.autoimplement.Mutator.getRandomExpression;
 
 import com.github.javaparser.ast.expr.Expression;
 
-public class Crossover {
-    public static Expression crossover(Expression mum, Expression dad) {
-        Expression baby;
-
-        if (Math.random() > 0.5) {
-            baby = mum.clone();
-            Expression mumExp = getRandomExpression(baby);
-            Expression dadExp = getRandomExpression(dad);
-            baby.replace(mumExp, dadExp);
-        } else {
-            baby = dad.clone();
-            Expression dadExp = getRandomExpression(baby);
-            Expression mumExp = getRandomExpression(mum);
-            baby.replace(dadExp, mumExp);
-        }
-        return baby;
-    }
+public class Crossover
+{
+	public static Expression crossover(Expression mum, Expression dad)
+	{
+		Expression baby;
+		
+		if (Math.random() > 0.5)
+		{
+			baby = mum.clone();
+			Expression mumExp = getRandomExpression(baby);
+			Expression dadExp = getRandomExpression(dad);
+			baby.replace(mumExp, dadExp);
+		}
+		else
+		{
+			baby = dad.clone();
+			Expression dadExp = getRandomExpression(baby);
+			Expression mumExp = getRandomExpression(mum);
+			baby.replace(dadExp, mumExp);
+		}
+		return baby;
+	}
 }
